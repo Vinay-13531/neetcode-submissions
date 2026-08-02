08430b65a9d8209b65c37c9970cf549243c3eba7
Vinay Narayanan <67695011+Vinay-13531@users.noreply.github.com>
2026-08-02 14:49:54 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        #return Counter(s) == Counter(t)    
+        
+        counts = [0] * 26
+        for char_s, char_t in zip(s,t):
+            counts[ord(char_s) - 97] += 1
+            counts[ord(char_t) - 97] -= 1
+        return all(letter == 0 for letter in counts)
+        
